63acae86f0f208aa93651c1b62b809fd1743d4c9
lizzyrcowan <ecowan@ucsd.edu>
2014-02-19 17:35:34 -0800

    first commit

---
+++ routes/index.js
@@ -0,0 +1,10 @@
+// Get all of our friend data
+var data = require('../data.json');
+
+exports.view = function(req, res){
+	res.render('index');
+};
+
+exports.register = function(req, res){
+	res.render('registration');
+};
